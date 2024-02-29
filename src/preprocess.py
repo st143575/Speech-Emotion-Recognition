faf29f37f119bd4b9cb6e87f37e55d15b4b605a2
Chong Shen <st143575@stud.uni-stuttgart.de>
2024-02-29 11:29:42 +0100

    Update src/preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,33 +1,16 @@
-import os
-import torch
+import os, argparse, torch
 import pandas as pd
-from torch.utils.data import DataLoader, Dataset
+from pathlib import Path
+from torch.utils.data import Dataset
+from utils_data import SERDataset
 from torch.utils.data import ChainDataset
 
-torch.manual_seed(12)
-
-PATH = os.getenv('PATH')
-DATA_PATH = os.getenv('DATA_PATH')
-
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(DEVICE)
-
-# Load training data
-ser_train = pd.read_json(DATA_PATH + 'train.json')
-ser_train = ser_train.transpose()
-
-# Load dev data
-ser_dev = pd.read_json(DATA_PATH + 'dev.json')
-ser_dev = ser_dev.transpose()
-ser_dev = ser_dev[['valence', 'activation', 'features']]
-
-# Load test data for submission test (ser_test_1.json)
-ser_test_1 = pd.read_json(DATA_PATH + 'ser_test_1.json')
-ser_test_1 = ser_test_1.transpose()
-# Add a placehoder -1 for valence and activation
-ser_test_1.insert(0, 'valence', -1)
-ser_test_1.insert(1, 'activation', -1)
-
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description='Preprocess data for SER')
+    parser.add_argument('-i', '--input_dir', type=str, default='../dataset', help='Path to the raw dataset')
+    parser.add_argument('-o', '--output_dir', type=str, default='./data', help='Path to save the preprocessed data')
+    return parser.parse_args()
 
 class SERDataset(Dataset):
     """
@@ -53,22 +36,50 @@ class SERDataset(Dataset):
         }
         return sample
 
+def main():
+    args = parse_arguments()
+    input_dir = Path(args.input_dir)
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    print("Input directory: ", input_dir, "\n")
+    print("Output directory: ", output_dir)
+    
+    # Load training data
+    ser_train = pd.read_json(f"{input_dir}/train.json")
+    ser_train = ser_train.transpose()
 
-# Instantiate the Dataset object for train and dev splits.
-train_set = SERDataset(ser_train)
-dev_set = SERDataset(ser_dev)
-test_set_1 = SERDataset(ser_test_1)
+    # Load dev data
+    ser_dev = pd.read_json(f"{input_dir}/dev.json")
+    ser_dev = ser_dev.transpose()
+    ser_dev = ser_dev[['valence', 'activation', 'features']]
 
-# print('train:\n', train_set.__getitem__(0), '\n')
-# print('dev:\n', dev_set.__getitem__(0), '\n')
-# print('test:\n', test_set_1.__getitem__(0), '\n')
+    # Load test data for submission test (ser_test_1.json)
+    ser_test_1 = pd.read_json(f"{input_dir}/ser_test_1.json")
+    ser_test_1 = ser_test_1.transpose()
+    # Add a placehoder -1 for valence and activation
+    ser_test_1.insert(0, 'valence', -1)
+    ser_test_1.insert(1, 'activation', -1)
 
-merged_set = ChainDataset([train_set, dev_set, test_set_1])
-dataset = {
-    'train': train_set, 
-    'dev': dev_set, 
-    'test': test_set_1,
-    }
+    # Instantiate the Dataset object for train and dev splits.
+    train_set = SERDataset(ser_train)
+    dev_set = SERDataset(ser_dev)
+    test_set_1 = SERDataset(ser_test_1)
 
-# Save dataset to disk
-torch.save(dataset, './data/dataset.pt')
+    # print('train:\n', train_set.__getitem__(0), '\n')
+    # print('dev:\n', dev_set.__getitem__(0), '\n')
+    # print('test:\n', test_set_1.__getitem__(0), '\n')
+
+    merged_set = ChainDataset([train_set, dev_set, test_set_1])
+    dataset = {
+        'train': train_set, 
+        'dev': dev_set, 
+        'test': test_set_1,
+        }
+
+    # Save dataset to disk
+    torch.save(dataset, f"{output_dir}/dataset.pt")
+    print("Data preprocessing done!")
+
+
+if __name__ == "__main__":
+    main()
